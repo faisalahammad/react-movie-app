902d57dd36dc607fa5aa56a44bd2f5535a1e2b52
Faisal Ahammad <faisalahammad24@gmail.com>
2022-01-28 23:47:40 +0600

    RMP - Display movie information

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='title'>React Movie Search</h1>
-      <SearchMovies></SearchMovies>
+      <SearchMovies />
     </div>
   );
 }
--- src/SearchMovies/SearchMovies.js
+++ src/SearchMovies/SearchMovies.js
@@ -15,22 +15,40 @@ const SearchMovies = () => {
       const res = await fetch(url);
       const data = await res.json();
       setMovies(data.results);
+      console.log(data.results);
     } catch (err) {
       console.log(err);
     }
   }
   return (
-    <div>
+    <>
       <form className="form" onSubmit={searchMovie}>
         <label className="label" htmlFor="query">Movie Name</label>
         <input className='input' name='query' type="text"
-          placeholder='i.e. Hello Dolly'
+          placeholder='i.e. Prison Break'
           value={query} onChange={(e) => setQuery(e.target.value)}
         />
-
         <button type="submit" className='button'>Search</button>
       </form>
-    </div>
+      <div className="card-list">
+        {
+          // remove movie if poster not found by filtering
+          movies.filter(movie => movie.poster_path).map(movie => (
+            <div className="card" key={movie.id}>
+              <img className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + ' poster'} />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p><small>Release Date: {movie.release_date}</small></p>
+                <p><small>Rating: {movie.vote_average}</small></p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 };
 
